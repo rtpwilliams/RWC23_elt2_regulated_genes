aa5dd9c5d60f41f0add445a2543481ecb35f7b87
Robert Williams <rtpwilliams@gmail.com>
2020-04-05 15:48:29 -0600

    Moved functions to file `RWC23_Functions.R`. Subset plots based on differential expression in pairwise elt-2 (-) and/or elt-7 (-) vs wt RNAseq.

--- RWC23_Functions.R
+++ RWC23_Functions.R
@@ -8,20 +8,35 @@ matrix_select <- function(count_matrix, gene_subset_vector){
   return(nameMatrix)
 }
 
-# row_normalize_matrix() subsets the count matrix based on value in variance_cutoff
+# row_normalize_matrix() performs a row normalization by subtracting the row mean from each value in the row.
+
+row_normalize_matrix <- function(count_matrix){
+  namevarRowNormalized <- count_matrix - rowMeans(count_matrix)
+  return(namevarRowNormalized)
+}
+
+# row_normalize_matrix_cutoff() subsets the count matrix based on value in variance_cutoff
 # It then performs a row normalization by subtracting the row mean from each value in the row.
 
-row_normalize_matrix <- function(count_matrix, variance_cutoff){
+row_normalize_matrix_cutoff <- function(count_matrix, variance_cutoff){
   varselect <- rowVars(count_matrix) >= variance_cutoff
   count_matrix <- count_matrix[varselect, ]
   namevarRowNormalized <- count_matrix - rowMeans(count_matrix)
   return(namevarRowNormalized)
-  #head(nameMatrix)
 }
 
-# row_zscore_matrix() assigns a row Z score to each row value.
+# row_zscore_matrix() assigns a row Z score to each row value. 
 
-row_zscore_matrix <- function(count_matrix, variance_cutoff){
+row_zscore_matrix <- function(count_matrix){
+  namevarZscore <- (count_matrix - rowMeans(count_matrix))/rowSds(count_matrix)
+  return(namevarZscore)
+  
+}
+
+# row_zscore_matrix_cutoff() assigns a row Z score to each row value. 
+# Then uses variance to subset data.
+
+row_zscore_matrix_cutoff <- function(count_matrix, variance_cutoff){
   varselect <- rowVars(count_matrix) >= variance_cutoff
   count_matrix <- count_matrix[varselect, ]
   namevarZscore <- (count_matrix - rowMeans(count_matrix))/rowSds(count_matrix)
@@ -43,4 +58,14 @@ id2name <- function(count_matrix){
     match(rownames(count_matrix), id2name.df$wbps_gene_id)
     ]
   return(count_matrix)
+}
+
+# myPheatmap() is a custom pheatmap plotting function
+
+myPheatmap <- function(count_matrix){
+  pheatmap(count_matrix, 
+           cluster_cols = FALSE, 
+           cluster_rows = TRUE, 
+           show_rownames = FALSE, 
+           border_color = NA)
 }
